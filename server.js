ff3e50d466936ead3ba3e70ce55668f3ab75d7ef
vuhn16 <vuhn16@gmail.com>
2021-10-17 21:55:06 +0700

    update server

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require('path');
 const ngApp = express();
 ngApp.use(express.static('./dist/angular-forms-validation'));
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/angular-forms-validation/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/training-angular-app/index.html'));
 });
 ngApp.listen(process.env.PORT || 8080);
